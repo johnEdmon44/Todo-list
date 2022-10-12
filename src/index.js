3abd92ee131ca3992105c5e20e100e1cd094fb4a
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-13 00:15:08 +0800

    Refactor modal

--- src/index.js
+++ src/index.js
@@ -2,30 +2,31 @@ import './style.css';
 
 'use strict';
 
-//Task modal
-const openTask = document.querySelector('#open-task');
-const taskModal = document.querySelector('#task-modal');
-const closeTask = document.querySelector('#close-task');
+(function () {
+  const openProject = document.querySelector('#open-project');
+  const projectModal = document.querySelector('#project-modal');
+  const closeProject = document.querySelector('#close-project');
+  const openTask = document.querySelector('#open-task');
+  const taskModal = document.querySelector('#task-modal');
+  const closeTask = document.querySelector('#close-task');
 
-openTask.addEventListener('click', () => {
-  taskModal.classList.add('visible');
-  projectModal.classList.remove('visible');
-});
+  //Project modal
+  openProject.addEventListener('click', () => {
+    projectModal.classList.add('visible');
+    taskModal.classList.remove('visible');
+  });
 
-closeTask.addEventListener('click', () => {
-  taskModal.classList.remove('visible');
-});
+  closeProject.addEventListener('click', () => {
+    projectModal.classList.remove('visible');
+  });
 
-//Project modal
-const openProject = document.querySelector('#open-project');
-const projectModal = document.querySelector('#project-modal');
-const closeProject = document.querySelector('#close-project');
+  //Task modal
+  openTask.addEventListener('click', () => {
+    taskModal.classList.add('visible');
+    projectModal.classList.remove('visible');
+  });
 
-openProject.addEventListener('click', () => {
-  projectModal.classList.add('visible');
-  taskModal.classList.remove('visible');
-});
-
-closeProject.addEventListener('click', () => {
-  projectModal.classList.remove('visible');
-});
+  closeTask.addEventListener('click', () => {
+    taskModal.classList.remove('visible');
+  });
+})();
