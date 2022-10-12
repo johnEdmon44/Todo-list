33b0643bc13b45ff277b3c78a2a03a97777adb8d
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-12 23:37:57 +0800

    Add task modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
 'use strict';
+
+const openTask = document.querySelector('#open-task');
+const taskModal = document.querySelector('#task-modal');
+const closeTask = document.querySelector('#close-task');
+
+openTask.addEventListener('click', () => {
+  taskModal.classList.add('visible');
+});
+
+closeTask.addEventListener('click', () => {
+  taskModal.classList.remove('visible');
+});
