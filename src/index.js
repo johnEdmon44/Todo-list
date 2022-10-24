1775daee48e66e76b6dc1ed4993bef5b2342e5aa
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-24 14:10:44 +0800

    Clean code

--- src/index.js
+++ src/index.js
@@ -7,26 +7,3 @@ import { project } from './function';
 
 //run tab switch
 project.tabSwitch();
-
-
-const projectList = [
-  {
-    title: 'Library',
-    task: [{id: 1, name: 'Improve UI'}, {id: 2, name: 'Refactor'}]
-  }, {
-    title: 'Restaurant',
-    task: [{id: 3, name: 'Improve UI'}, {id: 4, name: 'Refactor'}]
-  }
-]
-
-let name = 'Library'
-
-let hi = projectList.find(p => p.title === name)
-let hello = hi.task.find(t => t.id === 2)
-hi.task.splice(hello, 1)
-
-console.log(projectList)
-
-
-const inbox = document.querySelector('#inbox');
-console.log(inbox.id)
